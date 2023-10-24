ffdf4288b4ae6de4a3fff7f58b43e22d45f4d90e
Karen <92551451+karencardoz@users.noreply.github.com>
2023-10-24 15:47:40 -0400

    using the yargs module for note CLI

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 #!/usr/bin/env node
 
-console.log('Hello World')
-// console.log(global)
-// console.log(process.argv)
-// execute JS with node outside of a browser
-// TO EXECUTE: node index.js
-// this will give a Reference Error
-// alert('hello world')
-
-// import { sayHEllo } from './module.js';
-// sayHEllo()
-// which node
-// add bin -> note cli to package.json and run npm link
-// which note
-// add hashbang to tell we're running NodeJS #!/usr/bin/env node
-// run note and see Hello World, Voila!!
+import './src/command.js'
